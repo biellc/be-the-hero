c86872292aec6d3d909678245af98ba7f550a60c
BuildTools <unconfigured@null.spigotmc.org>
2020-03-25 16:51:54 -0300

    Back-End da aplicação

---
+++ backend/src/anotacoes.js
@@ -0,0 +1,51 @@
+/*
+ * Rota: localhost:3333
+ * Recurso: /*
+ */
+
+/**
+ * Métodos HTTP:
+ * 
+ * GET: Buscar uma informação do back-end
+ * POST: Criar uma informação no back-end
+ * PUT: Alterar uma informação no back-end
+ * DELETE: Deletar uma informação no back-end
+ */
+
+/**
+ * Tipos de parâmetros:
+ * 
+ * Query Params: Parâmetros nomeados enviados na rota após "?" (Filtros, paginação)
+ * Route Params: Parâmetros utilizados para identificar recursos
+ * Request Body: Corpo da requisição, utilizado para criar ou alterar recursos
+ */
+
+/**
+ * SQL: MySQL, SQLite, PostgreSQL, Oracle, Microsoft SQL Server
+ * NoSQL: MongoDB, CouchDB, etc
+ */
+
+/**
+ * Driver: SELECT * FROM users
+ * Query Builder: table('users).select('*').where()
+ */
+
+/**
+ * Entidades:
+ * 
+ * ONG
+ * Casos (incident)
+ */
+
+/**
+ * Funcionalidades:
+ * 
+ * Login de ONG
+ * Cadastro de ONG
+ * Logout de ONG
+ * Cadastrar novos casos
+ * Deletar casos
+ * Listar casos específicos de uma ONG
+ * Listar todos os casos
+ * Entrar em contato com a ONG
+ */
